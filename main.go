84f2374137aae501dfdcb2920cc2f329e270bd34
GT020 <gauravtanwar0020@gmail.com>
2024-06-25 00:10:10 +0530

    add docker file

--- main.go
+++ main.go
@@ -4,11 +4,13 @@ import (
 	"star-citizen/routes"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
 )
 
 func main() {
 
 	app := fiber.New()
+	app.Use(logger.New())
 
 	routes.Setup(app)
 
